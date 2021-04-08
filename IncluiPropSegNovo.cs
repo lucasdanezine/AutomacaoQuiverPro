8cad2d43da4f8c9895b1a159be3d930b8adc9833
Lucas gimenes <ldanezine@gmail.com>
2021-04-08 13:33:31 -0300

    Criado classe e desenvolvido código para chegar até o botão incluir novo seguro.

---
+++ IncluiPropSegNovo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
+using QuiverPro.ClassesNavega;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support;
+
+namespace QuiverPro
+{
+    /// <summary>
+    /// Descrição resumida para IncluiPropSegNovo
+    /// </summary>
+    [TestClass]
+    public class IncluiPropSegNovo
+    {
+
+        #region Atributos de teste adicionais
+        //
+        // É possível usar os seguintes atributos adicionais enquanto escreve os testes:
+        //
+        // Use ClassInitialize para executar código antes de executar o primeiro teste na classe
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup para executar código após a execução de todos os testes em uma classe
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize para executar código antes de executar cada teste 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup para executar código após execução de cada teste
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void IncluirSegNovo()
+        {
+            #region Classes utilizadas para login e navegação ate o modulo.
+            //Classe que faz login no sistema.
+            DadosLogin login = new DadosLogin();
+            //Classe instanciada para os metodos de navegação.
+            NavegaMenu navega = new NavegaMenu();
+            #endregion
+
+            //login no sistema.
+            IWebDriver driver = login.LogarNoSistema();
+            //Esperando tela de inicio carregar.
+            System.Threading.Thread.Sleep(8000);
+
+            //navega ao menu operacional.
+            driver = navega.NavegaOperacional(driver);
+
+            //comando para acesso ao iframe 
+            driver.SwitchTo().Frame("ZonaInterna");
+            //escolhendo o menu por javascript executando função diretamente.
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("SelecionaModuloJQuery('ConsultaEmissaoERecusa;Fast/FrmCadastroNovo.aspx?pagina=Documento','EMISSOESRECUSAS','Professional','EMISSOESRECUSAS','Propostas/Apólices'); ");
+            System.Threading.Thread.Sleep(5000);
+
+            driver.FindElement(By.Id("BtIncluir")).Click();
+
+        }
+    }
+}
